af466f7e2f7b41ec818fd6147d977d221f68aa11
unknown <bradmb90@yahoo.com>
2015-03-05 14:13:20 -0500

    Created a script and gameobject that will make a wall see through when the player enters the room

---
+++ Assets/Scripts/Wall_Fade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall_Fade : MonoBehaviour {
+
+	public GameObject wall;
+	public Material faded;
+	public Material normal;
+	public Renderer rend;
+	public bool insideRoom;
+
+	// Use this for initialization
+	void Start () {
+		rend = wall.GetComponent<Renderer> ();
+		rend.material = normal;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if(insideRoom){
+			insideRoom = false;
+			rend.material = normal;
+		}
+		else{
+			insideRoom = true;
+			rend.material = faded;
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
