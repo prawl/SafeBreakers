0eaf1071d43227132f9fdbd8e71d61766c7bf586
Patrick <ptyrawl@gmail.com>
2015-04-17 12:40:55 -0400

    t # This is a combination of 2 commits.
    Debug menu now displays useful development info [#92581622]

---
+++ Assets/Scripts/Developer/GUIDebug.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIDebug : MonoBehaviour {
+	private float buttonHeightSpacing = 35f;
+	private float buttonWidth = 400f;
+	private float buttonHeight = 35f;
+	private float boxWidth = 400f;
+	private float boxHeight = 200f;
+	private float boxHeightSpacing = 200f;
+	private float xPos;
+	private float yPos;
+    private float debugBoxHeight = 165f;
+  	private bool visible = true;
+	private static int roundedRestSeconds;
+	private static int displaySeconds;
+	private static int displayMinutes;
+	private static string debugText;
+  	private float[] coords;
+  	private Texture2D godModeButton;
+
+  void Start(){
+    godModeButton = Resources.Load("god_mode_image") as Texture2D; //Looks in Resources folder to images to load
+  }
+
+  void OnGUI(){
+    if(DebugMenuVisibile()){
+      DisplayDebugMenu();
+    }
+
+    coords = SetCoords(0, Screen.height - buttonHeightSpacing);
+    if (GUI.Button(new Rect(coords[0], coords[1], buttonWidth, buttonHeight), "Debug")){
+      ToggleDebugMenu();
+    }
+  }
+
+  public void DisplayDebugMenu(){
+    coords = SetCoords(0, Screen.height - boxHeightSpacing);
+    GUI.Box(new Rect(coords[0], coords[1], boxWidth, boxHeight), "");
+		GUI.TextField (new Rect(coords[0], coords[1], boxWidth, debugBoxHeight), DebugText(), 250);
+    GUI.backgroundColor = new Color(0,0,0,0);
+    coords = SetCoords( Screen.width - (Screen.width /3), 0);
+    if(GUI.Button(new Rect(coords[0], coords[1], 35, 35), godModeButton)){
+      PlayerController.ToggleGodMode();
+    }
+  }
+
+  public bool DebugMenuVisibile(){
+    return visible;
+  }
+
+  public void ToggleDebugMenu(){
+    if(DebugMenuVisibile()){
+      visible = false;
+    } else {
+      visible = true;
+    }
+  }
+
+  private string DebugText(){
+		roundedRestSeconds = Mathf.CeilToInt(Time.time);
+		displaySeconds = roundedRestSeconds % 60;
+		displayMinutes = roundedRestSeconds / 60;
+		debugText = string.Format ("Timer: {0:00}:{1:00}", displayMinutes, displaySeconds);	
+		debugText += "\n\n";
+		debugText += "Steps Taken: " + PlayerController.StepsTaken();	
+		debugText += "\n\n";
+		debugText += "Currency: " + InventoryController.DisplayCurrency();	
+		debugText += "\n\n";
+		debugText += "God Mode: " + PlayerController.GodMode();	
+    return debugText;
+  }
+
+  // Makes setting GUI objects x and y coordinates more efficient
+  private float[] SetCoords(float xPos, float yPos){
+    float[] coords = new float[] { xPos, yPos };
+    return coords;
+  }
+}
