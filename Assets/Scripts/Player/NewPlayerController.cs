8a123d1419661f740cc843fb10909313ce9484cd
Patrick Rawlings <patrick.rawlings@smashingboxes.com>
2015-08-07 15:44:38 -0400

    Refactor to shorten Player Controller

--- Assets/Scripts/Player/NewPlayerController.cs
+++ Assets/Scripts/Player/NewPlayerController.cs
@@ -7,36 +7,43 @@ using Rotorz.Tile.Internal;
 [RequireComponent(typeof(Animator))]
 public class NewPlayerController : MonoBehaviour {
 
-	public GameObject player;
-	public TileSystem tileSystem;
-	public TileIndex current, next;
-	public Camera camera;
-	private Vector3 temp;
-	private CharacterController controller;
-	private float speed = 1;
-	private bool moving;
-	private bool mouseInput;
+  public GameObject player;
+  public TileSystem tileSystem;
+  public TileIndex current, next;
+  public Camera camera;
+  private Vector3 temp;
+  private CharacterController controller;
+  private float speed = 1;
+  private bool moving;
+  private bool mouseInput;
 
-	// Use this for initialization
-	void Start () {
+  void Start(){
     EnableMouseInput();
-		player = GameObject.FindGameObjectWithTag ("Player");
-		GetComponent<Renderer>().castShadows = true;
-		GetComponent<Renderer>().receiveShadows = true;
-		controller = gameObject.GetComponent<CharacterController> ();
-		moving = false;
-		current = tileSystem.ClosestTileIndexFromWorld (transform.position);
-		temp = tileSystem.GetTile (current).gameObject.transform.position;
-		temp.y = 1;
-		transform.position = temp;
-		tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = true;
-	}
+    controller = gameObject.GetComponent<CharacterController> ();
+    InitializeShadows();
+    DisableMovement();
+    AttachPlayerToTile();
+  }
 
-	void Update() {
-		GetValidTiles();
-		GetNextTile();
-		DefaultTiles();
-	}
+  void Update(){
+    GetValidTiles();
+    GetNextTile();
+    DefaultTiles();
+  }
+
+  void AttachPlayerToTile(){
+    player = GameObject.FindGameObjectWithTag ("Player");
+    current = tileSystem.ClosestTileIndexFromWorld (transform.position);
+    temp = tileSystem.GetTile (current).gameObject.transform.position;
+    temp.y = 1;
+    transform.position = temp;
+    tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = true;
+  }
+
+  void InitializeShadows(){
+    GetComponent<Renderer>().castShadows = true;
+    GetComponent<Renderer>().receiveShadows = true;
+  }
 
   public bool Moving(){
     return moving;
@@ -49,7 +56,7 @@ public class NewPlayerController : MonoBehaviour {
   public void EnableMovement(){
     moving = true;
   }
-	
+
   public bool AllowMouseInput(){
     return mouseInput;
   }
@@ -62,7 +69,7 @@ public class NewPlayerController : MonoBehaviour {
     mouseInput = true;
   }
 
-	void GetNextTile(){
+  void GetNextTile(){
     if(Input.GetMouseButtonDown(0) && AllowMouseInput()) {
       Ray ray = camera.ScreenPointToRay (Input.mousePosition);
       RaycastHit hit;
@@ -72,70 +79,70 @@ public class NewPlayerController : MonoBehaviour {
     if(tileSystem.GetTile(next).gameObject.GetComponent<TileCheck>().valid){
       MoveToNextTile();
     }
-	}
+  }
 
-	public bool V3Equal(Vector3 a, Vector3 b){
-		return Vector3.SqrMagnitude (a - b) < 0.001;
-	}
+  public bool V3Equal(Vector3 a, Vector3 b){
+    return Vector3.SqrMagnitude (a - b) < 0.001;
+  }
 
-	void MoveToNextTile(){
+  void MoveToNextTile(){
     DisableMouseInput();
-		Vector3 nextLoc = tileSystem.GetTile (next).gameObject.transform.position;
-		nextLoc.y = 1;
-		if(!V3Equal (nextLoc, transform.position)){
+    Vector3 nextLoc = tileSystem.GetTile (next).gameObject.transform.position;
+    nextLoc.y = 1;
+    if(!V3Equal (nextLoc, transform.position)){
       EnableMovement();
-			Vector3 dir = (nextLoc - transform.position).normalized;
-			dir *= Time.fixedDeltaTime * speed;
-			controller.Move (dir);
-		}
-		if(V3Equal(nextLoc, transform.position)){
-			tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = false;
-			current = next;
-			tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = true;
-			transform.position = nextLoc;
+      Vector3 dir = (nextLoc - transform.position).normalized;
+      dir *= Time.fixedDeltaTime * speed;
+      controller.Move (dir);
+    }
+    if(V3Equal(nextLoc, transform.position)){
+      tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = false;
+      current = next;
+      tileSystem.GetTile (current).gameObject.GetComponent<TileCheck>().occupied = true;
+      transform.position = nextLoc;
       DisableMovement();
       EnableMouseInput();
-		}
-	}
+    }
+  }
 
-	void DefaultTiles(){
-		if(moving){
-			for(int i = 0; i < tileSystem.RowCount; i++){
-				for(int j = 0; j < tileSystem.ColumnCount; j ++){
-					if(tileSystem.GetTile (i, j) != tileSystem.GetTile(next)){
-						tileSystem.GetTile (i, j).gameObject.GetComponent<TileCheck>().valid = false;
-					}
-				} 
-			}
-		}
-	}
+  void DefaultTiles(){
+    if(moving){
+      for(int i = 0; i < tileSystem.RowCount; i++){
+        for(int j = 0; j < tileSystem.ColumnCount; j ++){
+          if(tileSystem.GetTile (i, j) != tileSystem.GetTile(next)){
+            tileSystem.GetTile (i, j).gameObject.GetComponent<TileCheck>().valid = false;
+          }
+        }
+      }
+    }
+  }
 
-	void GetValidTiles(){
-		if(!moving){
-			try{
-				if(!tileSystem.GetTile (current.row + 1, current.column).gameObject.GetComponent<TileCheck>().occupied){
-					tileSystem.GetTile (current.row + 1, current.column).gameObject.GetComponent<TileCheck>().valid = true;
-				}
-			}
-			catch{}
-			try{
-				if(!tileSystem.GetTile (current.row - 1, current.column).gameObject.GetComponent<TileCheck>().occupied){
-					tileSystem.GetTile (current.row - 1, current.column).gameObject.GetComponent<TileCheck>().valid = true;
-				}
-			}
-			catch{}
-			try{
-				if(!tileSystem.GetTile (current.row, current.column + 1).gameObject.GetComponent<TileCheck>().occupied){
-					tileSystem.GetTile (current.row, current.column + 1).gameObject.GetComponent<TileCheck>().valid = true;
-				}
-			}
-			catch{}
-			try{
-				if(!tileSystem.GetTile (current.row, current.column - 1).gameObject.GetComponent<TileCheck>().occupied){
-					tileSystem.GetTile (current.row, current.column - 1).gameObject.GetComponent<TileCheck>().valid = true;
-				}
-			}
-			catch{}
-		}
-	}
+  void GetValidTiles(){
+    if(!moving){
+      try{
+        if(!tileSystem.GetTile (current.row + 1, current.column).gameObject.GetComponent<TileCheck>().occupied){
+          tileSystem.GetTile (current.row + 1, current.column).gameObject.GetComponent<TileCheck>().valid = true;
+        }
+      }
+      catch{}
+      try{
+        if(!tileSystem.GetTile (current.row - 1, current.column).gameObject.GetComponent<TileCheck>().occupied){
+          tileSystem.GetTile (current.row - 1, current.column).gameObject.GetComponent<TileCheck>().valid = true;
+        }
+      }
+      catch{}
+      try{
+        if(!tileSystem.GetTile (current.row, current.column + 1).gameObject.GetComponent<TileCheck>().occupied){
+          tileSystem.GetTile (current.row, current.column + 1).gameObject.GetComponent<TileCheck>().valid = true;
+        }
+      }
+      catch{}
+      try{
+        if(!tileSystem.GetTile (current.row, current.column - 1).gameObject.GetComponent<TileCheck>().occupied){
+          tileSystem.GetTile (current.row, current.column - 1).gameObject.GetComponent<TileCheck>().valid = true;
+        }
+      }
+      catch{}
+    }
+  }
 }
